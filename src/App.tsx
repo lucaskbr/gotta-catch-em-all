ecfb8e3eb591ec0973970aeced2d8102520f745b
lucaskbr <lucas_klasa@hotmail.com>
2020-05-02 18:10:01 -0300

    feat: add redux store to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
+import store from './store';
 
 const App: React.FC = () => (
-  <>
+  <Provider store={store}>
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
     <GlobalStyle />
-  </>
+  </Provider>
 );
 
 export default App;
