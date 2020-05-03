2d5e2726fe1ce8475bf7cefd5e094f0a48d1dad5
lucaskbr <lucas_klasa@hotmail.com>
2020-05-03 00:14:28 -0300

    style: change home page style

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const PokemonList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`;
